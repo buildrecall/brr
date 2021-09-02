b79cd04b494580206ec154e6e977a846872d65d8
Kevin King <4kevinking@gmail.com>
2021-09-02 12:04:01 -0700

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use anyhow::Result;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");//
+    watch_dir();
 }
 
-fn watch_dir() {
-    
+fn watch_dir() -> Result<()> {
+    // Automatically select the best implementation for your platform.
+    let mut watcher = notify::recommended_watcher(|res| match res {
+        Ok(event) => println!("event: {:?}", event),
+        Err(e) => println!("watch error: {:?}", e),
+    })?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+
+    Ok(())
 }
