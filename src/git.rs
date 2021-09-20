55c0ba9e355636ec08115176622427b04b6039b9
Evan Conrad <evan@roomservice.dev>
2021-09-20 14:40:07 -0700

    reorg

--- src/git.rs
+++ src/git.rs
@@ -4,8 +4,10 @@ use std::{
 };
 
 use anyhow::{anyhow, Context, Result};
+use git2::{PushOptions, RemoteCallbacks};
+use uuid::Uuid;
 
-use crate::global_config::read_global_config;
+use crate::{global_config::read_global_config, worker_client::init_git_transport};
 
 fn copy_folder<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<()> {
     let mut stack = Vec::new();
@@ -52,7 +54,7 @@ fn copy_folder<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<()> {
     Ok(())
 }
 
-pub fn worktree_path(global_config_dir: PathBuf, project_id: uuid::Uuid) -> Result<PathBuf> {
+fn worktree_path(global_config_dir: PathBuf, project_id: uuid::Uuid) -> Result<PathBuf> {
     let config = read_global_config(global_config_dir.clone())?;
     let repoconfig = config
         .repo_config_by_id(project_id)
@@ -61,26 +63,95 @@ pub fn worktree_path(global_config_dir: PathBuf, project_id: uuid::Uuid) -> Resu
     Ok(path::Path::new(&repoconfig.path).to_path_buf())
 }
 
-pub fn repo_path(global_config_dir: PathBuf, project_id: uuid::Uuid) -> Result<PathBuf> {
+fn repo_path(global_config_dir: PathBuf, project_id: uuid::Uuid) -> Result<PathBuf> {
     Ok(global_config_dir.join(".gits").join(project_id.to_string()))
 }
 
-pub fn create_shadow_git_folder(global_config_dir: PathBuf, project_id: uuid::Uuid) -> Result<()> {
-    // Create the .git
-    let new_path = repo_path(global_config_dir.clone(), project_id)?;
-    let repo = git2::Repository::init_bare(new_path)?;
+pub struct RecallGit {
+    global_config_dir: PathBuf,
+}
 
-    // Populate the work tree
-    // let tree = repo
-    //     .worktree(
-    //         "buildrecall",
-    //         path::Path::new(&worktree_path(global_config_dir.clone(), project_id)?),
-    //         None,
-    //     )
-    //     .context("Failed to create the git worktree")?;
+impl RecallGit {
+    pub fn new(global_config_dir: PathBuf) -> Result<RecallGit> {
+        tracing_subscriber::fmt::init();
+        init_git_transport();
 
-    // fs::create_dir_all(new_path.clone())?;
-    // copy_folder(, new_path)?;
+        Ok(RecallGit {
+            global_config_dir: global_config_dir,
+        })
+    }
 
-    Ok(())
+    pub fn create_shadow_git_folder(&self, project_id: uuid::Uuid) -> Result<()> {
+        // Create the .git
+        let new_path = repo_path(self.global_config_dir.clone(), project_id)?;
+        let repo = git2::Repository::init_bare(new_path)?;
+
+        Ok(())
+    }
+
+    pub async fn push_project(&self, project_id: Uuid) -> Result<()> {
+        let config = read_global_config(self.global_config_dir.clone())?;
+        let dot_git_path = repo_path(self.global_config_dir.clone(), project_id.clone())
+            .context("Failed to create path")?;
+
+        let repo_exists = Path::new(&dot_git_path).is_dir();
+        let repo = match repo_exists {
+            true => git2::Repository::open(&dot_git_path)
+                .context(format!("Failed to open repo {:?}", dot_git_path))?,
+            false => git2::Repository::init_bare(dot_git_path.as_path())
+                .context("Failed to init repo")?,
+        };
+
+        repo.set_workdir(
+            &worktree_path(self.global_config_dir.clone(), project_id)?,
+            false,
+        )?;
+
+        Ok(tokio::runtime::Handle::current()
+            .spawn_blocking(move || -> Result<_> {
+                let mut i = repo.index().context("Failed to get a git index")?;
+
+                let tree = {
+                    i.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
+                    i.write_tree()
+                }?;
+
+                let tree = repo
+                    .find_tree(tree)
+                    .context("Failed to find a git tree in this repository")?;
+                let sig = repo.signature().context(
+                "failed to create a git signature (needed to make a commit in the shadow git repo)",
+            )?;
+
+                repo.commit(None, &sig, &sig, "sync with buildrecall", &tree, &[])
+                    .context("Failed to commit to the shadow git project")?;
+
+                let mut push_cbs = RemoteCallbacks::new();
+                push_cbs.push_update_reference(|ref_, msg| {
+                    eprintln!("{:?}", (ref_, msg));
+                    Ok(())
+                });
+
+                let remote_url = format!("{}/push", config.scheduler_host());
+                let mut push_opts = PushOptions::new();
+                push_opts.remote_callbacks(push_cbs);
+                let mut remote = repo
+                    .remote_anonymous(remote_url.clone().as_str())
+                    .context("Failed to create an anonymous remote in the shadow git project")?;
+
+                //  push to non-main branch so that we dont get "branch is currently checked out" error
+                //  https://stackoverflow.com/questions/2816369/git-push-error-remote-rejected-master-master-branch-is-currently-checked
+                let refspecs: &[&str] = &["+HEAD:refs/heads/incoming"];
+                remote
+                    .push(refspecs, Some(&mut push_opts))
+                    .context(format!(
+                        "Failed to push to the shadow git project with remote: {}",
+                        remote_url
+                    ))?;
+
+                Ok(())
+            })
+            .await
+            .context("Failed to spawn the tokio runtime")??)
+    }
 }
--- src/push.rs
+++ src/push.rs
@@ -1,88 +1,20 @@
-use anyhow::{anyhow, Context, Result};
-use git2::{IndexAddOption, RepositoryInitOptions};
-use std::{
-    env,
-    path::{self, Path, PathBuf},
-};
+use anyhow::{anyhow, Result};
+use std::path::PathBuf;
 
-use crate::{
-    git::{repo_path, worktree_path},
-    global_config::read_global_config,
-    worker_client::{init, init_git_transport},
-};
+use crate::{git::RecallGit, global_config::read_global_config};
 
 pub async fn run_push(global_config_dir: PathBuf) -> Result<()> {
-    use git2::{PushOptions, RemoteCallbacks};
-
-    init()?;
-
-    //  push to non-main branch so that we dont get "branch is currently checked out" error
-    //  https://stackoverflow.com/questions/2816369/git-push-error-remote-rejected-master-master-branch-is-currently-checked
-    let refspecs: &[&str] = &["+HEAD:refs/heads/incoming"];
-
     let config = read_global_config(global_config_dir.clone())?;
     let repoconfig = config
         .clone()
         .repo_config_of_current_dir()?
         .ok_or(anyhow!(
-            "This isn't a buildrecall project, run attach first."
+            "This isn't a buildrecall project, try running attach first."
         ))?
         .clone();
 
-    let dot_git_path =
-        repo_path(global_config_dir.clone(), repoconfig.id).context("Failed to create path")?;
-    let repo_exists = Path::new(&dot_git_path).is_dir();
-    let repo = match repo_exists {
-        true => git2::Repository::open(&dot_git_path)
-            .context(format!("Failed to open repo {:?}", dot_git_path))?,
-        false => {
-            git2::Repository::init_bare(dot_git_path.as_path()).context("Failed to init repo")?
-        }
-    };
+    let g = RecallGit::new(global_config_dir)?;
+    g.push_project(repoconfig.id).await?;
 
-    repo.set_workdir(&worktree_path(global_config_dir, repoconfig.id)?, false)?;
-
-    Ok(tokio::runtime::Handle::current()
-        .spawn_blocking(move || -> Result<_> {
-            let mut i = repo.index().context("Failed to get a git index")?;
-
-            let tree = {
-                i.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
-                i.write_tree()
-            }?;
-
-            let tree = repo
-                .find_tree(tree)
-                .context("Failed to find a git tree in this repository")?;
-            let sig = repo.signature().context(
-                "failed to create a git signature (needed to make a commit in the shadow git repo)",
-            )?;
-
-            repo.commit(None, &sig, &sig, "sync with buildrecall", &tree, &[])
-                .context("Failed to commit to the shadow git project")?;
-
-            let mut push_cbs = RemoteCallbacks::new();
-            push_cbs.push_update_reference(|ref_, msg| {
-                eprintln!("{:?}", (ref_, msg));
-                Ok(())
-            });
-
-            let remote_url = format!("{}/push", config.scheduler_host());
-            let mut push_opts = PushOptions::new();
-            push_opts.remote_callbacks(push_cbs);
-            let mut remote = repo
-                .remote_anonymous(remote_url.clone().as_str())
-                .context("Failed to create an anonymous remote in the shadow git project")?;
-
-            remote
-                .push(refspecs, Some(&mut push_opts))
-                .context(format!(
-                    "Failed to push to the shadow git project with remote: {}",
-                    remote_url
-                ))?;
-
-            Ok(())
-        })
-        .await
-        .context("Failed to spawn the tokio runtime")??)
+    Ok(())
 }
