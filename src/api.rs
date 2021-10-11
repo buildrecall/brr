8506ee157655ef76c4dfcc2e7024f1364e14c79e
Kevin King <4kevinking@gmail.com>
2021-10-11 14:11:16 -0700

    add tcp_keepalive for long builds

--- src/api.rs
+++ src/api.rs
@@ -284,7 +284,9 @@ impl BuildRecall for ApiClient {
 
         handle
             .spawn_blocking(move || -> Result<bool> {
-                let client = reqwest::blocking::Client::new();
+                let client = reqwest::blocking::ClientBuilder::new()
+                    .tcp_keepalive(Some(std::time::Duration::from_secs(60)))
+                    .build()?;
 
                 let resp = client
                     .get(artifact_url)
